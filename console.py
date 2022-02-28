47f0d3c303ceda5e36f7450bccf90144f50e85f9
Kaylee Dao <kayleed2@gmail.com>
2022-02-28 15:02:00 -0600

    Updated do_create, Updated AUTHORS

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
+from posixpath import split
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,13 +116,36 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        split_args = args.split()
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif split_args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        elif len(split_args) == 1:
+            new_instance = HBNBCommand.classes[split_args[0]]()
+            storage.save()
+            print(new_instance.id)
+            storage.save()
+            return
+        new_instance = HBNBCommand.classes[split_args[0]]()
+        if split_args[1] is not None:
+            for i in range(1, len(split_args)):
+                key_val = split_args[i].partition('=')
+                new_key = key_val[0]
+                new_val = key_val[2]
+                if '\"' in new_val:
+                    new_val = new_val[1:-1]
+                    new_val = new_val.replace("_", " ")
+                elif '.' in new_val:
+                    new_val = float(new_val)
+                else:
+                    new_val = int(new_val)
+                setattr(new_instance, new_key, new_val)
+                
+        else:
+            print("Not enough args")
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +296,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +304,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
