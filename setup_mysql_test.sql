383d8b28047f058ff48b72921a01b6668e8c40d5
Tim <tsimms@gmail.com>
2022-02-28 15:16:47 -0600

    script to setup test database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Creates a MySql server with
+-- database hbnb_dev_db
+-- user hbnb_dev (in localhost)
+-- password of hbnb dev is hbnb_dev_pwd
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;
