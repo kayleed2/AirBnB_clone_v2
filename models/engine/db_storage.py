6504c40355085579dc356f66f1607ada404bc920
Kaylee Dao <kayleed2@gmail.com>
2022-03-04 09:34:14 -0600

    pep8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -78,4 +78,4 @@ class DBStorage:
         """Creates all tables in database"""
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
-                                 expire_on_commit=False))
+                                        expire_on_commit=False))
