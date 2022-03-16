7ac714218179576eadb05f4fbb744c18451dc4f4
Kaylee Dao <kayleed2@gmail.com>
2022-03-16 18:02:32 -0500

    Using converted variable

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from tkinter import N
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    new = text.replace("_", " ")
+    return 'C %s' % new
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text=None):
+    if text:
+        if text is not None:
+            new = text.replace("_", " ")
+            return 'Python %s' % new
+    else:
+        return 'Python %s' % "is cool"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000)
