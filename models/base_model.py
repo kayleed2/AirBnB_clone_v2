e0b07af30d401f8c5a7b65bcf80843c81ff060e5
Kaylee Dao <kayleed2@gmail.com>
2022-03-18 08:56:21 -0500

    Changing base init to check for times and class

--- models/base_model.py
+++ models/base_model.py
@@ -18,17 +18,21 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
+        if id not in kwargs:
             self.id = str(uuid.uuid4())
+        if "created_at" not in kwargs:
             self.created_at = datetime.now()
+        else:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+        if "update_at" not in kwargs:
             self.updated_at = datetime.now()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+        if "__class__" in kwargs:
             del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
