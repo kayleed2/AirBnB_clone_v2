96768c556f30c6d3db84b711b9eb17824a416835
Kaylee Dao <kayleed2@gmail.com>
2022-03-21 10:10:03 -0500

    Sorting

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -16,13 +16,9 @@ def teardown_appcontext(response_or_exc):
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
-    amenity_names = []
     states = storage.all(State)
     cities = storage.all(City)
-    amenities = storage.all(Amenity).values()
-    for v in amenities:
-        amenity_names.append(v.name)
-    amenities = sorted(amenity_names)
+    amenities = storage.all(Amenity)
     return render_template('10-hbnb_filters.html', states=states,
                            cities=cities, amenities=amenities)
 
